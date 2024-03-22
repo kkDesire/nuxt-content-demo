ad607f07a2e92c65a0cd5cb743ef4cc80aacbb49
KOUASSI Désiré <desire.kouassi@mobisoft.ci>
2024-03-22 10:52:01 +0000

    first commit

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="max-w-3xl px-4 mx-auto">
+    <!-- Begin ::: Header ::: -->
+    <header class="mt-8 mb-4 flex justify-between items-end">
+      <div>
+        <h1 class="text-2xl font-semibold">
+          <NuxtLink to="/">Personnal Blog</NuxtLink>
+        </h1>
+        <p class="text-gray-500">Learn programming easly</p>
+      </div>
+      <Btn>Hire Me</Btn>
+    </header>
+    <!-- End   ::: Header ::: -->
+
+    <!-- Begin ::: Main Content ::: -->
+    <NuxtPage />
+    <!-- End   ::: Main Content ::: -->
+  </div>
+</template>
+<style>
+body {
+  @apply bg-gray-100;
+}
+</style>
---
+++ components/content/Btn.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="py-2 px-4 bg-white rounded-xl border my-2">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
